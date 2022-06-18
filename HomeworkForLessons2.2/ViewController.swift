3d2a910aa1701c0f6075412714f2d86ae7b365c2
Fedor Eronin <eroninf@icloud.com>
2022-06-18 15:40:27 +0300

    Initial Commit

---
+++ HomeworkForLessons2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkForLessons2.2
+//
+//  Created by Федор Еронин on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
