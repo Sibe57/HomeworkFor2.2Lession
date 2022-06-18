4e67813c2de2c54ffba4bdf3c768efb4c9bda52f
Fedor Eronin <eroninf@icloud.com>
2022-06-18 17:08:49 +0300

    add main functional and setup UI

--- HomeworkForLessons2.2/ViewController.swift
+++ HomeworkForLessons2.2/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var redLight: UIView!
+    var trafficLightState: TraficLightState = .off
+    
+    enum TraficLightState {
+        case off
+        case red
+        case yellow
+        case green
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightState = .off
+        setupLights()
+        setupButton()
     }
 
+    @IBAction func changeButtonTapped() {
+        switch trafficLightState {
+        case .off:
+            trafficLightState = .red
+            redLight.alpha = 1
+        case .red:
+            trafficLightState = .yellow
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case .yellow:
+            trafficLightState = .green
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+        case .green:
+            trafficLightState = .red
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        }
+    }
+    private func setupLights() {
+        redLight.layer.cornerRadius = 64
+        yellowLight.layer.cornerRadius = 64
+        greenLight.layer.cornerRadius = 64
+        redLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+    }
+    
+    private func setupButton() {
+        changeButton.setTitle("START", for: .normal)
+        changeButton.layer.cornerRadius = 10
+    }
+    
 
 }
 
